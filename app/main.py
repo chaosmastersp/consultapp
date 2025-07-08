1c13552f2f06053537d7442cf87b3b75b71d9c00
chaosmastersp <kojitarefas@gmail.com>
2025-07-08 20:59:06 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# FastAPI app principal
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "API online"}
+
+@app.get("/docs")
+def redirect_to_docs():
+    from fastapi.responses import RedirectResponse
+    return RedirectResponse(url="/docs")
