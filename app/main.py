3f163a78ffec4bea7566764b3c63d63d9b359983
chaosmastersp <kojitarefas@gmail.com>
2025-07-08 22:06:46 -0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
-from app.routers import auth, marcas, upload, consulta
+from app.routers import auth, marca, upload, consulta
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
 app.include_router(auth.router)
-app.include_router(marcas.router)
+app.include_router(marca.router)
 app.include_router(upload.router)
 app.include_router(consulta.router)
 
 @app.get("/")
 def root():
-    return {"status": "API online"}
+    return {"message": "API online"}
